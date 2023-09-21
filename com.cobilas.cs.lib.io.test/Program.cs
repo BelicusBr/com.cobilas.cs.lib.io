7c333700138506b6bcf2b2f226a0d3e9b6e6ab93
BelicusBr <x70r@outlook.com>
2023-09-21 01:24:16 -0300

    #Update-vs2.1.0

---
+++ com.cobilas.cs.lib.io.test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Cobilas.IO.Serialization.Json;
+
+namespace com.cobilas.cs.lib.io.test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Json.Serialize(new just { num1 = "Olá", num2 = "Mundo" }));
+
+            _ = Console.ReadLine();
+        }
+    }
+
+    [Serializable]
+    class just {
+        public string num1;
+        //[NonSerialized] 
+        public string num2;
+    }
+}
